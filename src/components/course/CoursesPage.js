48680d78a091433acdcfada2e3a889ed8721d430
Meital <zask.mz@gmail.com>
2017-01-31 15:47:16 +0200

    redux

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,13 +1,68 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import * as courseActions from '../../actions/courseActions';
+import {bindActionCreators} from 'redux';
 
 class CoursesPage extends React.Component{
+  constructor(props, context){
+    super(props, context);
+
+    this.state = {
+      course: { title: '' }
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event){
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({ course: course });
+  }
+
+  onClickSave(){
+    this.props.actions.createCourse(this.state.course);
+  }
+
+  courseRow(course, index){
+    return <div key={index}>{course.title}</div>;
+  }
+
   render(){
     return(
       <div>
-        <h1>some text here</h1>
+        <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.title} />
+
+        <input
+          type="submit"
+          onClick={this.onClickSave} />
       </div>
     );
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  actions: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired
+};
+
+function mapDispatchToProps(dispatch){
+  return{
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+function mapStateToProps(state, ownProps){
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CoursesPage extends React.Component{
+  render(){
+    return(
+      <div>some text here</div>
+    );
+  }
+}
+
+export default CoursesPage;
