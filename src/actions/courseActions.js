9374f252f31c518d3d9d96904e24ba4d907c46e5
Meital <zask.mz@gmail.com>
2017-02-07 15:49:55 +0200

    Load courses - using redux thunk

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,4 +1,15 @@
 import * as types from './actionTypes';
-export function createCourse(course){
-  return { type: types.CREATE_COURSE, course: course };
+import courseApi from '../api/mockCourseApi';
+
+export function loadCoursesSuccess(courses){
+  return { type: types.LOAD_COURSES_SUCCESS, courses: courses };
+}
+export function loadCourses(){
+  return function(dispatch){
+    return courseApi.getAllCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      throw(error);
+    });
+  };
 }
